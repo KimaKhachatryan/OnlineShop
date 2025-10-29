ee0d93fac0d26b1e3c19b3cc8e245ac9b0fc1fb3
kima.khachatryan <kima.khachatryan@softconstruct.com>
2025-10-29 08:38:39 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace OnlineShop;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.Run();
+    }
+}
